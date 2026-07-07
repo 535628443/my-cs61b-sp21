b1d02feb973cbeb51c62184fe0416b57bcd095dc
535628443 <535628443@qq.com>
2026-07-07 15:45:55 +0800

    解决了随机测试中的缩容问题

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -2,11 +2,63 @@ package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        correct.addLast(5);
+        correct.addLast(10);
+        correct.addLast(15);
+
+        broken.addLast(5);
+        broken.addLast(10);
+        broken.addLast(15);
+
+        assertEquals(correct.size(), broken.size());
+
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+        assertEquals(correct.removeLast(), broken.removeLast());
+
+        System.out.println("testThreeAddThreeRemove 已经成功执行且所有断言通过！");
+    }
+
+    @Test
+    public void testRandomized() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 3);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                buggy.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                int buggysize = buggy.size();
+                System.out.println("size: " + size);
+                System.out.println("size: " + buggysize);
+            } else if (operationNumber == 2) {
+                // removeLast
+                if (L.size() > 0) {
+                    int removedL = L.removeLast();
+                    int removedBuggy = buggy.removeLast();
+                    assertEquals(removedL, removedBuggy);
+                }
+            }
+        }
+    }
 }
