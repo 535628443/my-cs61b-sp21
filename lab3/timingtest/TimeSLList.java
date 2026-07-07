bd8410646116e778f5937660ea002cb68efd2fce
535628443 <535628443@qq.com>
2026-07-07 11:44:36 +0800

    完成了 TimeSLList

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -22,7 +22,29 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        int M = 10000;
+        for (int N = 1000; N <= 128000; N *= 2) {
+            SLList<Integer> list = new SLList<>();
+            for (int i = 0; i < N; i += 1) {
+                list.addLast(i);
+            }
+
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < M; j += 1) {
+                list.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+
+            Ns.addLast(N);
+            times.addLast(timeInSeconds);
+            opCounts.addLast(M);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
