404c5a0f5b3cb98d4f981bedd96e756267d07168
535628443 <535628443@qq.com>
2026-07-12 17:31:23 +0800

    修复了LLD的性能问题

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -51,61 +51,58 @@ public class LinkedListDeque<T> implements Deque<T>{
     @Override
     public void printDeque() {
         // TODO: 打印队列中的元素
-        Node<T> p = sentinel;
-        while (p.next != null) {
-            p = p.next;
+        Node<T> p = sentinel.next; // 从真正的第一个元素开始
+        while (p != sentinel) {    // 遇到 sentinel 说明绕完一圈了
             System.out.print(p.item + " ");
+            p = p.next;
         }
+        System.out.println(); // 打印完毕后换行，让输出更美观
     }
 
     @Override
     public T removeFirst() {
         // TODO: 移除并返回队首元素
-        if (sentinel.next != null) {
-            T x = sentinel.next.item;
-            sentinel.next = sentinel.next.next;
-            size -= 1;
-            return x;
-        } else {
+        if (size < 1) {
             return null;
         }
+        T x = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel; // 新的队首向左指回 sentinel
+        size -= 1;
+        return x;
     }
 
     @Override
     public T removeLast() {
         // TODO: 移除并返回队尾元素
-        Node<T> p = sentinel;
-        if (sentinel.next != null) {
-            while (p.next != null){
-                p = p.next;
-            }
-            p.prev.next = null;
-            size -= 1; // 别忘了更新 size！
-            return p.item; // 直接返回 p.item，不需要定义 x
-        } else {
+        if (size < 1) {
             return null;
         }
+        T x = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel; // 新的队尾向右指回 sentinel
+        size -= 1;
+        return x;
     }
 
     @Override
     public T get(int index) {
         // TODO: 获取指定索引位置的元素
-        Node<T> p = sentinel;
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        Node<T> p = sentinel.next;
         for(int i = 0; i < index; i++) {
-            if (p.next != null) {
-                p = p.next;
-            } else {
-                return null;
-            }
+            p = p.next;
         }
         return p.item;
     }
 
     public T getRecursive(int index) {
-        if (index < 1 || index > size) {
+        if (index < 0 || index >= size) {
             return null;
         } else {
-            return getRecursiveHelper(sentinel.next, index-1);
+            return getRecursiveHelper(sentinel.next, index);
         }
     }
 
