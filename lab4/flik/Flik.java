c45b29e04f7e51e9698a2a634b1116565f40eff4
Andy Yang <535628443@qq.com>
2026-07-26 17:29:33 +0800

    修复了 Flik 中的 bug

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -1,5 +1,7 @@
 package flik;
 
+import java.util.Objects;
+
 /** An Integer tester created by Flik Enterprises.
  * @author Josh Hug
  * */
@@ -8,6 +10,17 @@ public class Flik {
      *  @param b Value 2
      *  @return Whether a and b are the same */
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        return Objects.equals(a, b);
+        // return a == b
+        /*
+        Integer 类型是对象 == 只能对比存入的地址
+        int 类型是整数 才能使用 == 判断大小
+
+        对于 Integer 对象创建时:
+        Java 会创建 -128 ~ 127 在一个地址范围内(正好占据一个byte)
+        i,j 从 0 到 127 都对比的是同一个地址
+        但是 128 超出了，Java 单独为 i,j 分别又创建了一个容纳 128 的地址
+        所以当对比 128 的时候，i,j 的地址已经完全不同了
+         */
     }
 }
--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -5,6 +5,8 @@ public class HorribleSteve {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
+            // Java 会自动把 int 转换成 Integer
+            // Java 自动执行 Integer.valueOf(int i)
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
