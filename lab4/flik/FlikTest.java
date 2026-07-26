a2eb66c013d626c19ee06a4a16627d1ebc5b2a69
Andy Yang <535628443@qq.com>
2026-07-26 22:10:18 +0800

    补充了测试文件

---
+++ lab4/flik/FlikTest.java
@@ -0,0 +1,15 @@
+package flik;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FlikTest {
+
+    @Test
+    public void testFlik() {
+        assertTrue(Flik.isSameNumber(127,127));
+        assertTrue(Flik.isSameNumber(128,128));
+        assertTrue(Flik.isSameNumber(500,500));
+    }
+}
+
